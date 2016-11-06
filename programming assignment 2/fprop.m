c30e0ac2e4f65a77e9059af23ebed3c9f81dde8b
Jihan Kim <n37jan@gmail.com>
2016-11-07 01:18:36 +0900

    Programming Assignment 2 done.

--- programming assignment 2/fprop.m
+++ programming assignment 2/fprop.m
@@ -40,14 +40,11 @@ numhid2 = size(embed_to_hid_weights, 2);
 
 %% COMPUTE STATE OF WORD EMBEDDING LAYER.
 % Look up the inputs word indices in the word_embedding_weights matrix.
-embedding_layer_state = reshape(...
-  word_embedding_weights(reshape(input_batch, 1, []),:)',...
-  numhid1 * numwords, []);
+embedding_layer_state = reshape(word_embedding_weights(reshape(input_batch, 1, []),:)', numhid1 * numwords, []);
 
 %% COMPUTE STATE OF HIDDEN LAYER.
 % Compute inputs to hidden units.
-inputs_to_hidden_units = embed_to_hid_weights' * embedding_layer_state + ...
-  repmat(hid_bias, 1, batchsize);
+inputs_to_hidden_units = embed_to_hid_weights' * embedding_layer_state + repmat(hid_bias, 1, batchsize);
 
 % Apply logistic activation function.
 % FILL IN CODE. Replace the line below by one of the options.
@@ -55,7 +52,7 @@ hidden_layer_state = zeros(numhid2, batchsize);
 % Options
 % (a) hidden_layer_state = 1 ./ (1 + exp(inputs_to_hidden_units));
 % (b) hidden_layer_state = 1 ./ (1 - exp(-inputs_to_hidden_units));
-% (c) hidden_layer_state = 1 ./ (1 + exp(-inputs_to_hidden_units));
+hidden_layer_state = 1 ./ (1 + exp(-inputs_to_hidden_units));
 % (d) hidden_layer_state = -1 ./ (1 + exp(-inputs_to_hidden_units));
 
 %% COMPUTE STATE OF OUTPUT LAYER.
@@ -63,7 +60,7 @@ hidden_layer_state = zeros(numhid2, batchsize);
 % FILL IN CODE. Replace the line below by one of the options.
 inputs_to_softmax = zeros(vocab_size, batchsize);
 % Options
-% (a) inputs_to_softmax = hid_to_output_weights' * hidden_layer_state +  repmat(output_bias, 1, batchsize);
+inputs_to_softmax = hid_to_output_weights' * hidden_layer_state +  repmat(output_bias, 1, batchsize);
 % (b) inputs_to_softmax = hid_to_output_weights' * hidden_layer_state +  repmat(output_bias, batchsize, 1);
 % (c) inputs_to_softmax = hidden_layer_state * hid_to_output_weights' +  repmat(output_bias, 1, batchsize);
 % (d) inputs_to_softmax = hid_to_output_weights * hidden_layer_state +  repmat(output_bias, batchsize, 1);
@@ -73,12 +70,10 @@ inputs_to_softmax = zeros(vocab_size, batchsize);
 % softmax unit does not affect the outputs. Here we are subtracting maximum to
 % make all inputs <= 0. This prevents overflows when computing their
 % exponents.
-inputs_to_softmax = inputs_to_softmax...
-  - repmat(max(inputs_to_softmax), vocab_size, 1);
+inputs_to_softmax = inputs_to_softmax - repmat(max(inputs_to_softmax), vocab_size, 1);
 
 % Compute exp.
 output_layer_state = exp(inputs_to_softmax);
 
 % Normalize to get probability distribution.
-output_layer_state = output_layer_state ./ repmat(...
-  sum(output_layer_state, 1), vocab_size, 1);
+output_layer_state = output_layer_state ./ repmat(sum(output_layer_state, 1), vocab_size, 1);
